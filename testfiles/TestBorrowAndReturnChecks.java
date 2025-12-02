7ab4293c69209966472d2915bfd3a167db44e281
jobanpreet1835 <jobanpreet1835@gmail.com>
2025-12-01 20:01:46 -0800

    Added Java test files

---
+++ testfiles/TestBorrowAndReturnChecks.java
@@ -0,0 +1,83 @@
+package lab5.tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import lab5.BorrowingService;
+import lab5.Ebook;
+import lab5.Member;
+import lab5.PaperBook;
+
+public class TestBorrowAndReturnChecks {
+	
+	Member member1;
+	Member member2;
+	
+	PaperBook book1 = new PaperBook("Harry Potter");
+	Ebook book2 = new Ebook("Lord of The Rings");
+	PaperBook book3 = new PaperBook("Percy Jackson");
+	Ebook book4 = new Ebook("James And The Giant Peach");
+
+	private BorrowingService service = BorrowingService.getInstance();
+	BorrowingService borrowingService = new BorrowingService();
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		member1 = new Member("Jane",service); // flush borrowedBook array 
+		member2 = new Member("Jimmy",service);   // flush borrowedBook array 
+		book1.setIsAvailable(true);
+		book2.setIsAvailable(true);
+		book3.setIsAvailable(true);
+		book4.setIsAvailable(true);
+	}
+	
+	@Test
+	void exceededBorrowLimit() {
+		System.out.println("Borrow limit Test:");
+		member1.borrowBook(book1);
+		member1.borrowBook(book2);
+		member1.borrowBook(book3);
+		member1.borrowBook(book4);
+	}
+	
+	@Test
+	void bookAlreadyBorrowed() {
+		System.out.println("Book already borrowed Test:");
+		member1.borrowBook(book1);
+		
+		member1.borrowBook(book1);
+		member2.borrowBook(book1);
+		
+	}
+	
+	@Test
+	void borrowABook() {
+		System.out.println("Borrow Test:");
+		member1.borrowBook(book1);
+		member2.borrowBook(book3);
+		//assertEquals(borrowingService.borrowBook(member1,book1), false, "Should be false");
+	}
+	
+	@Test
+	void returnABook() {
+		System.out.println("Return Test:");
+		member1.borrowBook(book2);
+		member1.returnBook(book2);
+		//assertEquals(borrowingService.returnBook(member2,book2), false, "Should be false");
+	}
+	
+	@Test
+	void noBookToReturn() {
+		System.out.println("No books to borrow Test:");
+		member2.returnBook(book3);
+	}
+	
+	@Test
+	void returnBookNotFound() {
+		System.out.println("Return book not found Test:");
+		member2.borrowBook(book1);
+		member2.returnBook(book3);
+	}
+}
---
+++ testfiles/TestFindClassString.java
@@ -0,0 +1,65 @@
+package lab5.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import lab5.PaperBook;
+import lab5.Book;
+import lab5.BorrowingService;
+import lab5.Library;
+import lab5.Member;
+
+
+class TestFindClassString {
+
+	
+private Library library;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		 this.library = new Library(); // empty library for each test
+	}
+
+	private BorrowingService service = BorrowingService.getInstance();
+	
+	Member member1 = new Member("Dude",service);
+	Member member2 = new Member("Gal",service);
+	Book book1 = new PaperBook("Dune");
+	Book book2 = new PaperBook("1984");
+
+	@Test
+	void FindMember() {
+		library.addMember(member1);
+		library.addMember(member2);
+		Member member = library.findMemberByName(member1.getName());
+		assertEquals(member, member1, "Found member doesn't match");
+		assertEquals(library.membersCount(), 2, "There should be two members");
+		library.removeMember(member2);
+		assertEquals(library.membersCount(), 1, "There should be only one member remain");
+		member = library.findMemberByName(member2.getName());
+		assertNull(member, "The mmber should not hav ben found after removal from the library");
+		library.removeMember(member2);
+		assertEquals(library.membersCount(),  1, "Removal of a non-existent member should not affect library");
+		library.removeMember(member1);
+		assertEquals(library.membersCount(), 0, "All members should have been removed");
+	}
+
+	@Test
+	void FindBook() {
+		library.addBook(book1);
+		library.addBook(book2);
+		Book book = library.findBookByTitle(book1.getTitle());
+		assert(book == book1);
+		assert(library.booksCount() == 2);
+		library.removeBook(book2);
+		assert(library.booksCount() == 1);
+		book = library.findBookByTitle(book2.getTitle());
+		assert(book == null);
+		library.removeBook(book2);
+		assert(library.booksCount() == 1);
+		library.removeBook(book1);
+		assert(library.booksCount() == 0);
+	}
+}
